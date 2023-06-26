e80c79dda498e2ed4270aaf763527f644bd71047
Emre Cetinkaya <emre.cetkaya@gmail.com>
2023-06-26 00:55:03 -0700

    Login has been created, admin panel structure has been created

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession()
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
-        </div>
-      </div>)
-  }
-
+  const { data: session } = useSession();
+  if (!session) return;
   return (
-    <div>Signed in as {session.user.email}</div>
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 text-black gap-1 rounded-lg overflow-hidden">
+          <img src={session?.user?.image} alt="user-image" className="w-6 h-6" />
+          <span className="px-2">
+            {session?.user?.name}
+          </span>
+        </div>
+      </div>
+    </Layout>
   )
 }
