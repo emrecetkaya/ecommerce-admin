d48ee379af3d5e1cad92aa1bfcfe1bb543766419
Emre Cetinkaya <emre.cetkaya@gmail.com>
2023-07-27 20:07:44 -0700

    Amazon s3 connected, images uploaded

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -8,7 +8,8 @@ export default function ProductForm(
     _id,
     title: existingTitle,
     description: existingDescription,
-    price: existingPrice
+    price: existingPrice,
+    images
   }
 ) {
   const [title, setTitle] = useState(existingTitle || '');
@@ -35,6 +36,21 @@ export default function ProductForm(
     router.push('/products');
   }
 
+  async function uploadImages(ev) {
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append('file', file);
+      }
+      const res = await fetch('/api/upload', {
+        method: 'POST',
+        body: data,
+      })
+      console.log(res);
+    }
+  }
+
   return (
     <form onSubmit={saveProduct}>
       <label>Product name</label>
@@ -44,6 +60,24 @@ export default function ProductForm(
         value={title}
         onChange={ev => setTitle(ev.target.value)}
       />
+      <label>
+        Photos
+      </label>
+      <div className="mb-2">
+        <label className="w-24 h-24 cursor-pointer text-center flex text-sm gap-1 text-gray-500 rounded-lg bg-gray-200 items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+          </svg>
+
+          <div>
+            Upload
+          </div>
+          <input type="file" onChange={uploadImages} className="hidden" />
+        </label>
+        {!images?.length && (
+          <div>No photos in this product</div>
+        )}
+      </div>
       <label>Description</label>
       <textarea
         placeholder="description"
