d856895f582ade7e0b4406a9141da481d82e4994
Emre Cetinkaya <emre.cetkaya@gmail.com>
2023-08-14 07:51:54 -0700

    Images being uploaded

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -9,18 +9,19 @@ export default function ProductForm(
     title: existingTitle,
     description: existingDescription,
     price: existingPrice,
-    images
+    images: existingImages
   }
 ) {
   const [title, setTitle] = useState(existingTitle || '');
   const [description, setDescription] = useState(existingDescription || '');
   const [price, setPrice] = useState(existingPrice || '');
   const [goToProducts, setGoToProducts] = useState(false);
+  const [images, setImages] = useState([]);
   const router = useRouter();
 
   const saveProduct = async (ev) => {
     ev.preventDefault();
-    const data = { title, description, price };
+    const data = { title, description, price, images };
 
     if (_id) {
       //Update
@@ -43,11 +44,10 @@ export default function ProductForm(
       for (const file of files) {
         data.append('file', file);
       }
-      const res = await fetch('/api/upload', {
-        method: 'POST',
-        body: data,
-      })
-      console.log(res);
+      const res = await axios.post('/api/upload', data);
+      setImages(oldImages => {
+        return [...oldImages, ...res.data.links];
+      });
     }
   }
 
@@ -63,7 +63,12 @@ export default function ProductForm(
       <label>
         Photos
       </label>
-      <div className="mb-2">
+      <div className="mb-2 flex flex-wrap gap-2">
+        {!!images?.length && images.map(link => (
+          <div key={link} className="h-24">
+            <img src={link} alt="" className="rounded-lg" />
+          </div>
+        ))}
         <label className="w-24 h-24 cursor-pointer text-center flex text-sm gap-1 text-gray-500 rounded-lg bg-gray-200 items-center justify-center">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
